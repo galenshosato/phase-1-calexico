0bdd00380992986c7a1bc7a8fde511c22230ca0c
Galen Sho Sato <galen.sato@gmail.com>
2023-01-13 17:13:13 -0500

    Finished the first three challenges

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your code here...
+
+let firstMenuItem;
+
+fetch('http://localhost:3000/menu')
+    .then(resp => resp.json())
+    .then(menuItems => menuItems.forEach(menuItem => {
+        if (firstMenuItem === undefined) {
+            firstMenuItem = menuItem
+            renderMenuItem(firstMenuItem)
+        }
+        
+        const menuItemSpan = document.createElement('span')
+        menuItemSpan.textContent = menuItem.name
+        menuItemSpan.addEventListener('click', (e) => {
+            renderMenuItem(menuItem)
+        })
+
+        document.querySelector('#menu-items').append(menuItemSpan)
+    
+    }))
+    
+
+function renderMenuItem(menuItem) {
+    const img = document.querySelector('#dish-image')
+    img.src = menuItem.image
+
+    const dishH3 = document.querySelector('#dish-name')
+    dishH3.textContent = menuItem.name
+
+    const dishDesc = document.querySelector('#dish-description')
+    dishDesc.textContent = menuItem.description
+
+    const dishPrice = document.querySelector('#dish-price')
+    dishPrice.textContent = menuItem.price
+
+}
